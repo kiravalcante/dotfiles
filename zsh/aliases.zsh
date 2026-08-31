0dd5ef3023a1255b9dd4ed27a093bad9275e2ece
kiravalcante <mcavalcante.deb@gmail.com>
2026-08-31 15:48:39 -0300

    2026-08-31

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,12 @@
 alias re='source ~/.zshrc'
+alias zshrc='nano ~/.zshrc'
+alias aliases='nano $ZSH_CUSTOM/aliases.zsh'
+
+alias sc='echo $?'
 
 alias sai='sudo apt install'
 alias sar='sudo apt remove'
+alias sal='sudo apt list'
+alias sas='sudo apt search'
+
+alias psswd='nano ~/senhas/senhas'
